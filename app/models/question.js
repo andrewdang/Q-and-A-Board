97a7876a7a40f1275af472f3c90fabbbee193633
andrewdang17 <andrew.dang17@gmail.com>
2015-09-27 13:59:46 -0700

    add sort by most recent question

--- app/models/question.js
+++ app/models/question.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   question: DS.attr(),
   user: DS.attr(),
-  date: DS.attr(),
+  date: DS.attr('date'),
   details: DS.attr(),
   answers: DS.hasMany('answer', {async: true})
 });
