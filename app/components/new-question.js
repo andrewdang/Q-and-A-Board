f1e911bb8000c104111767790702d80e45f0287f
andrewdang17 <andrew.dang17@gmail.com>
2015-09-27 13:13:24 -0700

    add new question component

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -4,12 +4,23 @@ export default Ember.Component.extend({
   newQuestionForm: false,
 
   actions: {
-    showQuestionForm: function() {
+    showQuestionForm() {
       this.set('newQuestionForm', true);
     },
 
-    hideQuestionForm: function() {
+    hideQuestionForm() {
       this.set('newQuestionForm', false);
+    },
+
+    saveQuestion() {
+      var params = {
+        question: this.get('question'),
+        user: this.get('user') ? this.get('user') : 'anonymous',
+        date: new Date(),
+        details: this.get('details'),
+      };
+      this.set('newQuestionForm', false),
+      this.sendAction('saveQuestion', params);
     }
   }
 });
