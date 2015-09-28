37c49d762618dc120877b39bdd267d23239d1750
andrewdang17 <andrew.dang17@gmail.com>
2015-09-27 21:57:59 -0700

    add # of answers to left of question

--- app/components/question-listing.js
+++ app/components/question-listing.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'span',
   sortedQuestionDates: ['date:desc'],
   sortedQuestions: Ember.computed.sort('questions', 'sortedQuestionDates')
 });
